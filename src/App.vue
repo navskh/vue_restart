0683c529823787bc42a1038939a14697c4c7a965
Young <navskh@naver.com>
2022-02-04 17:57:03 +0900

    All lecture is complete

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">  
+  <div id="app">
     <tool-bar></tool-bar>
     <!-- url이 만약 jobs다-->
     <transition name="page">
@@ -10,60 +10,63 @@
 </template>
 
 <script>
-import ToolBar from './components/ToolBar.vue';
-import Spinner from './components/Spinner.vue';
-import bus from './utils/bus';
+import ToolBar from "./components/ToolBar.vue";
+import Spinner from "./components/Spinner.vue";
+import bus from "./utils/bus";
 
 export default {
-  components:{
+  components: {
     ToolBar,
     Spinner,
   },
-  data(){
+  data() {
     return {
       loadingStatus: false,
     };
   },
-  methods:{
-    startSpinner(){
+  methods: {
+    startSpinner() {
       this.loadingStatus = true;
     },
-    endSpinner(){
+    endSpinner() {
       this.loadingStatus = false;
-    }
+    },
   },
-  created(){
-    bus.$on('start:spinner', this.startSpinner);
-    bus.$on('end:spinner', this.endSpinner);
+  created() {
+    console.log(process.env.VUE_APP_TITLE);
+    bus.$on("start:spinner", this.startSpinner);
+    bus.$on("end:spinner", this.endSpinner);
   },
-  beforeDestroy(){ // 이벤트 등록하고 나면 반드시 종료시켜줘야함.
-    bus.$off('start:spinner', this.startSpinner);
-    bus.$off('end:spinner', this.endSpinner);
-  }
-}
+  beforeDestroy() {
+    // 이벤트 등록하고 나면 반드시 종료시켜줘야함.
+    bus.$off("start:spinner", this.startSpinner);
+    bus.$off("end:spinner", this.endSpinner);
+  },
+};
 </script>
 
 <style>
-  body{
-    padding: 0;
-    margin: 0;
-  }
-  a{
-    color: #34495e;
-    text-decoration: none;
-  }
-  a:hover{
-    color:#42b803;
-    text-decoration: underline;
-  }
-  a.router-link-exact-active{
-    text-decoration: underline;
-  }
-  /* router transition */
-  .page-enter-active, .page-leave-active {
-    transition: opacity .5s;
-  }
-  .page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
-    opacity: 0;
-  }
+body {
+  padding: 0;
+  margin: 0;
+}
+a {
+  color: #34495e;
+  text-decoration: none;
+}
+a:hover {
+  color: #42b803;
+  text-decoration: underline;
+}
+a.router-link-exact-active {
+  text-decoration: underline;
+}
+/* router transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s;
+}
+.page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
